7d1893c43ea84855485dd0aca8a8973526e619d9
Freddie Lindsey <freddie.a.lindsey@gmail.com>
2015-06-02 16:35:32 +0100

    Draft instructions header for encode/decode

--- src/instructions.h
+++ src/instructions.h
@@ -3,14 +3,42 @@
 
 #include "emulate.h"
 
+/*
+ *  TYPEDEFS / STRUCTS / UNIONS
+ */
+
+typedef enum instruction_k {
+  dataproc = 0, multiply = 1, singledatatransfer = 2, branch = 3
+} instruction_kt;
+
+typedef struct decoded_instruction {
+  instruction_kt kind;
+  uint8_t cond;
+  uint8_t opcode;
+  uint8_t immediate;
+  uint8_t set;
+  uint8_t prepost;
+  uint8_t up;
+  uint8_t loadstore;
+  instruction_t regd;
+  instruction_t regs1;
+  instruction_t regs2;
+  instruction_t regs3;
+  instruction_t other;
+} decoded_instruction_t ;
+
+/*
+ *  FUNCTION PROTOTYPES
+ */
+
 /*  EXECUTOR */
-int instruction_execute(char** instruction_decoded, machine_t *machine);
+int instruction_execute(decoded_instruction_t *decoded, machine_t *machine);
 
 /*  ENCODER */
-
+instruction_t instruction_encode(decoded_instruction_t* decoded);
 
 /*  DECODER */
-char** instruction_decode(instruction_t instruction);
+decoded_instruction_t* instruction_decode(instruction_t instruction);
 
 /*  FETCH */
 instruction_t fetch_instruction(machine_t *machine);
