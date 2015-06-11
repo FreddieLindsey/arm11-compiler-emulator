3518b106bcd27a419971a229de7ea28b939be252
adamhosier <hosieradam@gmail.com>
2015-06-11 17:21:44 +0100

    assemble fixes & gpio formatting

--- programs/gpio.s
+++ programs/gpio.s
@@ -6,20 +6,24 @@ str r2,[r0]          ; sets up pin 16 as an output
 mov r1,#1
 lsl r1,#16           ; mask for pin 16
 
-mov r4,#2000000      ; store a very big number
-reset:
-mov r5,#0            ; store counter
-str r1,[r0,#40]      ; clear pin 16
-loopBody:
-add r5,#1            ; increment counter by 1
-cmp r4, r5           ; compare big number and counter
-blt loopBody         ; if counter is not equal to big number, loop again
-mov r5,#0            ; resets counter
-str r1,[r0,#28]      ; set pin 16
-loopBody2:
-add r5,#1	     ; increments counter by 1
-cmp r4, r5           ; compares big number and counter
-blt loopBody2        ; if counter is not equal to big number, loop again
-b reset              ; jump to reset
-andeq r0, r0, r0     ; halt
+ldr r4, =0x0FFFFFFF  ; store a very big number
 
+reset:
+  mov r5,#0          ; store counter
+  str r1,[r0,#40]    ; clear pin 16
+
+  loopBody:
+    add r5, r5, #1   ; increment counter by 1
+    cmp r4, r5       ; compare big number and counter
+    blt loopBody     ; if counter is not equal to big number, loop again
+
+  mov r5,#0          ; resets counter
+  str r1,[r0,#28]    ; set pin 16
+
+  loopBody2:
+    add r5, r5, #1   ; increments counter by 1
+    cmp r4, r5       ; compares big number and counter
+    blt loopBody2    ; if counter is not equal to big number, loop again
+
+;  b reset            ; jump to reset
+andeq r0, r0, r0     ; halt
